85ebbe148ac820c8d85f31056f5827021d158124
Sheila Diz <dizsheila07@gmail.com>
2023-12-16 23:11:13 -0300

    [Pulse] feat: create project with img and data

---
+++ Pulse/src/utils/data.js
@@ -0,0 +1,64 @@
+export const links = [
+    {
+        type: "link",
+        title:"Features",
+        url:"#features"
+    },
+    {
+        type: "link",
+        title:"Costumer Stories",
+        url:"#costumer-stories"
+    },
+    {
+        type: "link",
+        title:"Pricing",
+        url:"/"
+    },
+    {
+        type: "link",
+        title:"Blog",
+        url:"#blog"
+    },
+    {
+        type: "link",
+        title:"Contact",
+        url:"#contact"
+    },
+    {
+        type: "link",
+        title:"Support",
+        url:""
+    },
+    {
+        type: "button",
+        title:"Sign Up",
+        url:"#plus"
+    },
+];
+
+export const faq = [
+    {
+        question: "How does the 30-day free trial work?",
+        answer: ["You can use Pulse absolutely free for 30 days. After 30 days, we will charge the credit card you used when you sign up. If you choose to cancel your account before your trial ends, your card will not be charged."]
+    },
+    {
+        question: "If I upgrade an existing account, do I get another free trial?",
+        answer: ["Brand new accounts are the only ones eligible for a 30-day free trial. If you upgrade an existing account, your credit card will be billed the new amount on the date of your next billing cycle."]
+    },
+    {
+        question: "What is a “Financial Account”?",
+        answer: ["Financial Accounts are a grouping of income and expense items that can be used for tracking or reporting purposes.", "They’re designed to be flexible and can be used to differentiate between bank accounts, companies, individuals or between personal and business entries."]
+    },
+    {
+        question: "Do I have to commit to a long-term contract?",
+        answer: ["No. There is no contract when you use Pulse, other than agreeing to our Terms of Service."]
+    },
+    {
+        question: "Will I be able to cancel my account if I don’t use Pulse?",
+        answer: ["Yes – you can cancel your account at any time. To cancel your account, go to your billing page and click on “Please cancel my account” at the bottom of the page. The account owner is the only one who can cancel an account. An email request to delete an account is not considered cancellation."]
+    },
+    {
+        question: "What types of payments do you accept?",
+        answer: ["We accept payments made online using Visa, Mastercard, Discover, and American Express."]
+    }
+]
